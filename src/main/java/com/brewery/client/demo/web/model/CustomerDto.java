0114a31573c26cce475f11fe304d54a4c54b0d2d
hmnshgpt455 <hmnshgpt455@gmail.com>
2020-12-17 20:55:42 +0530

    Adding brewery client implementation - This will be use for reference

---
+++ src/main/java/com/brewery/client/demo/web/model/CustomerDto.java
@@ -0,0 +1,18 @@
+package com.brewery.client.demo.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerDto {
+
+    private UUID id;
+    private String name;
+}
---
+++ src/test/java/com/brewery/client/demo/web/client/BreweryClientTest.java
@@ -0,0 +1,47 @@
+package com.brewery.client.demo.web.client;
+
+import com.brewery.client.demo.web.model.BeerDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.net.URI;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BreweryClientTest {
+
+    @Autowired
+    BreweryClient breweryClient;
+
+    @Test
+    void getBeerById() {
+        BeerDto dto = breweryClient.getBeerById(UUID.randomUUID());
+        assertNotNull(dto);
+    }
+
+    @Test
+    void saveNewBeer() {
+        BeerDto beerDto = BeerDto.builder().build();
+
+        URI uri = breweryClient.saveNewBeer(beerDto);
+
+        assertNotNull(uri);
+
+        System.out.println(uri.toString());
+    }
+
+    @Test
+    void updateBeer() {
+        BeerDto beerDto = BeerDto.builder()
+                            .beerName("New Beer").build();
+        breweryClient.updateBeer(UUID.randomUUID(), beerDto);
+    }
+
+    @Test
+    void deleteBeer() {
+        breweryClient.deleteBeer(UUID.randomUUID());
+    }
+}
